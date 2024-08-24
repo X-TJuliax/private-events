2f83ec993648f0e0a3fb09b42665ebe832b95b59
a1808417 <zhubowen55@gmail.com>
2024-08-24 17:21:31 +0930

    finish first

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,25 @@
 class EventsController < ApplicationController
+    before_action :authenticate_user!
+
     def index
         @events = Event.all
+        @users = User.all
+        @event = current_user.events.build
+    end
+
+    def create
+       @event = current_user.events.build(events_params)
+       if @event.save
+          redirect_to root_path
+       else 
+          @events = Event.all
+          render :index
+       end
+    end
+
+    private
+
+    def events_params
+       params.require(:event).permit(:title, :date, :location, :description)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'events#index'
+  # get 'new_event', to: 'events#index', as: 'new_event'
+  post 'new_event', to: 'events#create', as: 'create_new_event'
 end
